598e210782ae5d387cf59496e5a9fa4b34ddf5f7
gori39 <shakeels1939@gmail.com>
2023-01-17 20:33:06 +0530

    Pizza1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,9 +5,13 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean CheeseEx;
+    private boolean Toppings;
+    private boolean TakeAway;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        price = this.isVeg?300:400;
         // your code goes here
     }
 
@@ -17,18 +21,32 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!CheeseEx){
+            CheeseEx=true;
+            price+=isVeg?70:120;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!Toppings){
+            Toppings=true;
+            price+=isVeg?70:120;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        // your code goes
+        if(!TakeAway){
+            TakeAway=true;
+            price+=20;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        bill = "Base Price Of The Pizza: "+(isVeg?300:400)+"\n"+(CheeseEx?("Extra Cheese Added: "+80+"\n"):"")+(Toppings?("Extra Toppings Added: "+(isVeg?70:120)+"\n"):"")+
+                (TakeAway?("Paperbag Added: "+20+"\n"):"")+"Total Price: "+price+"\n";
         return this.bill;
     }
 }
