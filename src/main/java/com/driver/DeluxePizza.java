c22a3c5bc3db4e40365748e4a122dfa075ca7187
gori39 <shakeels1939@gmail.com>
2023-01-17 21:00:16 +0530

    Pizza3

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,12 +1,11 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
+    int price;
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes
         addExtraCheese();
         addExtraToppings();
-
     }
 }
